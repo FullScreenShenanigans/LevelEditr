9983387d707e70b46017804dd3a85e496584e39d
Josh Goldberg <josh@fullscreenmario.com>
2015-10-05 19:35:58 -0700

    Added overridden areas to maps interface

--- Source/LevelEditr.d.ts
+++ Source/LevelEditr.d.ts
@@ -61,6 +61,9 @@ declare module LevelEditr {
 
     export interface IMapsCreatrMapRaw extends MapsCreatr.IMapsCreatrMapRaw {
         time: number;
+        areas: {
+            [i: string]: IMapsCreatrAreaRaw;
+        }
     }
 
     export interface IMapsCreatrAreaRaw extends MapsCreatr.IMapsCreatrAreaRaw {
